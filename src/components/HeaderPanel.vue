14c8e4bc190edb5a29ca05ab8e5abb594b4bc183
shj <kan02134@gmail.com>
2021-09-27 01:09:38 +0900

    chore: fix header title

--- src/components/HeaderPanel.vue
+++ src/components/HeaderPanel.vue
@@ -6,7 +6,7 @@
       src="../assets/logo.png"
     />
     <h1 class="font-black pt-6 text-4xl tracking-wider">
-      fruition-with-giscus
+      fruition-with-comments
     </h1>
     <h3 class="pt-2">
       Add a comments system using <b>Giscus</b> to a site built with
